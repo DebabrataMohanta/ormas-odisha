3083961f9b970cac3fa313ef8e0e1581d6abf50d
Deb <deb135936@gmail.com>
2023-01-20 19:18:58 +0530

    Ormas

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className="page-footer"
-    style={{display: "flex",justifyContent: "space-between", flexDirection:"row",display: "flex",paddingTop: "0px",backgroundColor:"#2F8614",color:"white"}}>
+    style={{display: "flex",justifyContent: "space-between", flexDirection:"row",paddingTop: "0px",backgroundColor:"#2F8614",color:"white"}}>
      
     <div>
       <span style={{paddingLeft:"30px", paddingTop: "10px"}}>Home | FAQ | Site Map | Contact Us</span>
@@ -15,7 +15,7 @@ export default function Footer() {
      
     <div>
       <span style={{textAlign:"right", padding: "1px",marginTop:"1px"}}>Powered By <a href="https://ijentech.com/"
-            style={{textDecoration: "none"}} target="_blank"><img src={tech} height="45px"/></a>
+             style={{textDecoration: "none"}} target="_blank"><img src={tech} height="45px"/></a>
       </span>
     </div>
   </footer>
